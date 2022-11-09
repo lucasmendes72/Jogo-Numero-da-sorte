8d111f0d2957537bdd413aad54d82e64fc57fc72
lucasmendes72 <113635393+lucasmendes72@users.noreply.github.com>
2022-11-09 10:18:18 -0300

    Atualizando validação

--- validacao.js
+++ validacao.js
@@ -1,43 +1,39 @@
-function verificaValidadeDoChute(chute) {
+function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
-    if(chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Número Inválido</div>'
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
         return
     }
 
-    if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTML += '<div>`Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}`</div>'
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
         return
     }
 
-    if(numero === numeroSecreto) {
+    if (numero === numeroSecreto) {
         document.body.innerHTML = `
-            <h2>Você Acertou!</h2>
+            <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     } else {
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
+ 
 }
 
-function chuteForInvalido() {
+function chuteForInvalido(numero) {
     return Number.isNaN(numero)
 }
 
-function numeroForMaiorOuMenorQueOValorPermitido(numero) {
-    return numero > maiorValor || numero < menorValor 
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
 }
-
-const jogarNovamente = document.getElementById('jogar-novamente');
-
-jogarNovamente.addEventListener('click', () => {
-    window.location.reload();
-})
